355ca46f54a612bf69a79bc69cb54922ca59efda
Debabrata Mondal <debabratajr@gmail.com>
2020-07-24 11:04:51 +0530

    update card design

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const card = ({ state, active, confirmed, deaths, recovered }) => {
             <div>
                 <h1 className='state'>{state}</h1>
                 <p className='confirmed'><span>Confirmed</span>{confirmed}</p>
-                <p className='active'><span>Active</span>{active}</p>
+                <p className='active-class'><span>Active</span>{active}</p>
                 <p className='deaths'><span>Recovered/Deaths</span>{recovered}/{deaths}</p>
             </div>
         </div>
